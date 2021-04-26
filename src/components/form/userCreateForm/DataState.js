fd1078b0380d399558011764b65e1b1a477b47e7
kanishka <mallawarachchikanishka@gmail.com>
2021-04-27 00:50:41 +0530

    completd GRN module, GTn IN and OUT module, user creation and update module...Huge work Fantastic Feeling Wow...

--- src/components/form/userCreateForm/DataState.js
+++ src/components/form/userCreateForm/DataState.js
@@ -7,4 +7,13 @@ const FORM_INITAL_VALUE = {
   status: "active",
 };
 
-export { FORM_INITAL_VALUE };
+const FORM_CLEAR = {
+  userId: "",
+  userName: "",
+  NIC: "",
+  location: "",
+  role: "",
+  status: "active",
+};
+
+export { FORM_INITAL_VALUE, FORM_CLEAR };
--- src/components/form/userCreateForm/FormOne.js
+++ src/components/form/userCreateForm/FormOne.js
@@ -1,153 +1,451 @@
-import React, { useState } from "react";
-import { axios } from "../../../connection/axios";
+import React, { useState, useEffect } from "react";
 import Grid from "@material-ui/core/Grid";
-import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
 import TextField from "@material-ui/core/TextField";
-import INITDATA from "../../../data/user.create.data";
+import Button from "@material-ui/core/Button";
+import { FORM_INITAL_VALUE, FORM_CLEAR } from "./DataState";
+import { Alert } from "@material-ui/lab";
+import { PORT, URL } from "../../../connection/defaultconfig";
+import { axios } from "../../../connection/axios";
 import {
   FormControl,
   FormControlLabel,
   FormLabel,
-  InputLabel,
   Radio,
   RadioGroup,
-  Select,
-  FormHelperText,
-  MenuItem,
-  Button,
+  InputAdornment,
+  Modal,
+  IconButton,
+  InputLabel,
+  Input,
+  Snackbar,
 } from "@material-ui/core";
+import MUIDataTable from "mui-datatables";
+import clsx from "clsx";
+import AddIcon from "@material-ui/icons/Add";
 
-export default function AddressForm(props) {
-  const { userName, NIC, location, userId, role, status } = props.userValues;
+const useStyles = makeStyles((theme) => ({
+  modal: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  left_margin_itmgrp: {
+    marginLeft: 80,
+  },
+  left_margin_sup: {
+    marginLeft: 60,
+  },
+  txt_userid: {
+    width: "26ch",
+  },
+  margin: {
+    margin: theme.spacing(0),
+  },
+  textField: {
+    width: "28ch",
+  },
+  textField_select: {
+    width: "22ch",
+  },
+  button: {
+    marginTop: theme.spacing(3),
+    marginLeft: theme.spacing(1),
+  },
+  buttons: {
+    display: "flex",
+    justifyContent: "flex-end",
+  },
+}));
 
-  const errors = props.userErrors;
+export default function Form_1() {
+  const classes = useStyles();
+  const [value, setValue] = useState(FORM_INITAL_VALUE);
 
-  const dirty = props.userDirty;
+  const responsive = "vertical";
+  const tableBodyHeight = "100%";
+  const tableBodyMaxHeight = "";
+  const [open, setOpen] = useState(false);
+  const [user_update, setUserUpdate] = useState(false);
+  const [disable_userid, setDisable_userid] = useState(true);
+  const [disable_location, setDisable_location] = useState(false);
+  const [disable_fields, setDisable_fields] = useState(true);
+  const [type, setType] = useState("location");
+  const [ModalOpen, setModalOpen] = useState(false);
+  const [severity, setSeverity] = useState("warning");
+  const [message, setMessage] = useState("");
+  const [error, setError] = useState({
+    NIC: false,
+  });
+  const [dirty] = useState({
+    NIC: "Invalid NIC Number",
+  });
 
-  const handleOnChange = props.handleChanges;
+  const location = [
+    ["100", "Head Office"],
+    ["10", "Nawinna"],
+    ["25", "Dehiwala SC"],
+  ];
 
-  console.log("Errors ==>", errors);
-  console.log("dirty ==>", dirty);
+  const role = [
+    ["1", "IT Officer"],
+    ["CD202", "Power Supply"],
+    ["CD201", "Point of sale machine 01"],
+    ["CD204", "Executive PC"],
+    ["CD255", "printer laserjet"],
+  ];
 
-  // const getUser = async () => {
-  //   const response = await axios
-  //     .get("/user/getuser/10/kumara")
-  //     .catch((err) => console.log("Error ", err));
-  //   if (response) {
-  //     console.log(response.data);
-  //   }
-  // };
+  const pickList_header = ["ID", "Name"];
 
-  /*Drop down list items mapping*/
-  const INITLOCATIONS = INITDATA.LOCATIONS.map((data) => (
-    <MenuItem key={data.title} value={data.title}>
-      {data.title}
-    </MenuItem>
-  ));
+  const handleRowClick = (dataIndex) => {
+    if (type === "location") {
+      setValue({ ...value, location: dataIndex[0] });
+    } else {
+      setValue({ ...value, role: dataIndex[0] });
+    }
 
-  const INITROLES = INITDATA.ROLES.map((data) => (
-    <MenuItem key={data.title} value={data.title}>
-      {data.title}
-    </MenuItem>
-  ));
+    pickListClose();
+  };
+
+  const picklistOptions = {
+    elevation: 1,
+    viewColumns: false,
+    filter: false,
+    print: false,
+    download: false,
+    filterType: "dropdown",
+    responsive,
+    tableBodyHeight,
+    tableBodyMaxHeight,
+    onRowsDelete: false,
+    selectableRows: "none",
+    onRowClick: handleRowClick,
+  };
+
+  function picklistData() {
+    if (type === "location") {
+      return location;
+    } else {
+      return role;
+    }
+  }
+
+  //check location value exsist to enable user id field
+
+  useEffect(() => {
+    if (value.location) {
+      setDisable_userid(false);
+    }
+  }, [value.location]);
+
+  const pickListOpen = (type) => {
+    setType(type);
+    setModalOpen(true);
+  };
+
+  const pickListClose = () => {
+    setModalOpen(false);
+  };
+
+  const handleChange = (props) => (event) => {
+    if (props === "NIC") {
+      if (/^([0-9]{9}[x|X|v|V]|[0-9]{12})$/.test(event.target.value)) {
+        setError({ ...error, [props]: false });
+        setValue({ ...value, [props]: event.target.value });
+      } else {
+        setError({ ...error, [props]: true });
+        setValue({ ...value, [props]: event.target.value });
+      }
+    } else {
+      setValue({ ...value, [props]: event.target.value });
+    }
+  };
+
+  const pushData = async () => {
+    axios
+      .post(`http://${URL}:${PORT}/user/createuser`, {
+        userid: value.userId,
+        name: value.userName,
+        nic: value.NIC,
+        password: "12345678",
+        roleid: value.role,
+        locationid: value.location,
+        status: value.status,
+        mod_by: "kanishka",
+        mod_date: "2021-04-18",
+        cre_by: "kanishka",
+        cre_date: "2021-04-18",
+      })
+      .then(
+        (response) => {
+          console.log(response);
+          setMessage("User Successfully created");
+          setSeverity("success");
+          setOpen(true);
+          handleClear();
+        },
+        (error) => {
+          setMessage("Unxepected Error");
+          setSeverity("error");
+          setOpen(true);
+          handleClear();
+          console.log(error);
+        }
+      );
+  };
+
+  const getData = async (name) => {
+    axios
+      .get(`http://${URL}:${PORT}/user/getuser/${value.location}/${name}`)
+      .then(
+        (response) => {
+          if (response.data !== "") {
+            console.log(response);
+            setUserUpdate(true);
+            setDisable_userid(true);
+            setDisable_location(true);
+            setDisable_fields(false);
+            setMessage("User Already Exsist");
+            setSeverity("info");
+            setOpen(true);
+            setValue({
+              ...value,
+              userName: response.data.name,
+              NIC: response.data.nic,
+              role: response.data.roleid,
+              location: response.data.locationid,
+              status: response.data.status,
+            });
+          } else {
+            console.log(response.data);
+            setDisable_fields(false);
+            setUserUpdate(false);
+          }
+        },
+        (error) => {
+          setMessage("Unxepected Error");
+          setSeverity("error");
+          setOpen(true);
+          handleClear();
+          console.log(error);
+        }
+      );
+  };
+
+  // update existing inventory item data
+  const updateData = async () => {
+    axios
+      .post(`http://${URL}:${PORT}/user/updateuser`, {
+        userid: value.userId,
+        name: value.userName,
+        nic: value.NIC,
+        roleid: value.role,
+        locationid: value.location,
+        status: value.status,
+        mod_by: "kanishka",
+        mod_date: "2021-04-18",
+      })
+      .then(
+        (response) => {
+          console.log(response);
+          setMessage(response.data);
+          setSeverity("success");
+          setOpen(true);
+          handleClear();
+        },
+        (error) => {
+          setMessage("Unxepected Error");
+          setSeverity("error");
+          setOpen(true);
+          handleClear();
+          console.log(error);
+        }
+      );
+  };
+
+  function checkUserId(event) {
+    const name = event.target.value;
+    if (name.trim() !== "") {
+      getData(name);
+    }
+  }
+
+  const handleSubmit = () => {
+    if (value.userId.trim() === "") {
+      setMessage("User ID is Required");
+      setOpen(true);
+    } else if (value.location === "") {
+      setMessage("Location code is Required");
+      setOpen(true);
+    } else if (value.role === "") {
+      setMessage("Role is Required");
+      setOpen(true);
+    } else if (value.userName.trim() === "") {
+      setMessage("User Name is Required");
+      setOpen(true);
+    } else if (value.NIC.trim() === "") {
+      setMessage("Nic No is Required");
+      setOpen(true);
+    } else {
+      if (user_update) {
+        updateData();
+      } else {
+        pushData();
+      }
+    }
+  };
+
+  function handleClear() {
+    setValue(FORM_CLEAR);
+
+    setUserUpdate(false);
+    setDisable_userid(true);
+    setDisable_fields(true);
+    setDisable_location(false);
+  }
+
+  const handleSnackBarClose = (reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+    setOpen(false);
+  };
+
+  const body = (
+    <div className={classes.paper_modal}>
+      <MUIDataTable
+        title={"Pick List"}
+        data={picklistData()}
+        columns={pickList_header}
+        options={picklistOptions}
+      />
+    </div>
+  );
 
   return (
     <React.Fragment>
-      <Typography variant="h6" gutterBottom>
-        User Form
-      </Typography>
+      <Modal className={classes.modal} open={ModalOpen} onClose={pickListClose}>
+        {body}
+      </Modal>
+
+      <Snackbar
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "right",
+        }}
+        open={open}
+        onClose={handleSnackBarClose}
+        autoHideDuration={2000}
+      >
+        <Alert onClose={handleSnackBarClose} severity={severity}>
+          {message}
+        </Alert>
+      </Snackbar>
       <Grid container spacing={3}>
-        <Grid item xs={12} sm={12}>
-          <InputLabel htmlFor="age-native-simple">Location</InputLabel>
-          <FormControl fullWidth>
-            <Select
-              autoFocus
-              fullWidth
-              name="location"
-              id="location"
-              value={location}
-              defaultValue=""
-              onChange={handleOnChange}
-            >
-              {INITLOCATIONS}
-            </Select>
-            <FormHelperText>
-              {Boolean(errors.location && dirty.location)
-                ? errors.location
-                : ""}
-            </FormHelperText>
+        <Grid item xs={3}>
+          <FormControl className={clsx(classes.margin, classes.textField)}>
+            <InputLabel htmlFor="standard-adornment-password">
+              Location
+            </InputLabel>
+            <Input
+              id="standard-adornment-password"
+              value={value.location}
+              disabled={disable_location}
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    disabled={disable_location}
+                    onClick={() => {
+                      pickListOpen("location");
+                    }}
+                  >
+                    <AddIcon />
+                  </IconButton>
+                </InputAdornment>
+              }
+            />
           </FormControl>
         </Grid>
-        <Grid item xs={12} sm={12}>
+        <Grid item xs={3} className={classes.left_margin_itmgrp}>
           <TextField
-            fullWidth
-            error={Boolean(errors.userId && dirty.userId)}
-            id="standard-basic"
+            className={classes.txt_userid}
+            disabled={disable_userid}
+            id="userid"
             label="User ID"
             name="userId"
-            value={userId}
-            onChange={handleOnChange}
-            helperText={
-              Boolean(errors.userId && dirty.userId) ? errors.userId : ""
-            }
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <TextField
-            fullWidth
-            error={Boolean(errors.userName && dirty.userName)}
-            id="standard-basic"
-            label="User Name"
-            name="userName"
-            value={userName}
-            onChange={handleOnChange}
-            helperText={
-              Boolean(errors.userName && dirty.userName) ? errors.userName : ""
-            }
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <TextField
-            fullWidth
-            error={Boolean(errors.NIC && dirty.NIC)}
-            id="standard-basic"
-            label="NIC"
-            name="NIC"
-            value={NIC}
-            onChange={handleOnChange}
-            helperText={Boolean(errors.NIC && dirty.NIC) ? errors.NIC : ""}
+            value={value.userId}
+            onChange={handleChange("userId")}
+            onBlur={checkUserId}
           />
         </Grid>
 
-        <Grid item xs={12}>
-          <InputLabel htmlFor="age-native-simple">Role</InputLabel>
-          <FormControl fullWidth error={Boolean(errors.role && dirty.role)}>
-            <Select
-              fullWidth
-              name="role"
-              id="role"
-              value={role}
-              defaultValue=""
-              onClick={handleOnChange}
-            >
-              {INITROLES}
-            </Select>
-            <FormHelperText>
-              {Boolean(errors.role && dirty.role) ? errors.role : ""}
-            </FormHelperText>
+        <Grid item xs={3} className={classes.left_margin_sup}>
+          <FormControl className={clsx(classes.margin, classes.textField)}>
+            <InputLabel htmlFor="standard-adornment-password">Role</InputLabel>
+            <Input
+              id="standard-adornment-password"
+              value={value.role}
+              disabled={disable_fields}
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    disabled={disable_fields}
+                    aria-label="toggle password visibility"
+                    onClick={() => {
+                      pickListOpen("role");
+                    }}
+                  >
+                    <AddIcon />
+                  </IconButton>
+                </InputAdornment>
+              }
+            />
           </FormControl>
         </Grid>
+
+        <Grid item xs={6}>
+          <TextField
+            fullWidth
+            disabled={disable_fields}
+            id="userName"
+            label="User name"
+            name="userName"
+            value={value.userName}
+            onChange={handleChange("userName")}
+          />
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <TextField
+            fullWidth
+            disabled={disable_fields}
+            error={error.NIC}
+            id="NIC"
+            label="NIC"
+            name="NIC"
+            value={value.NIC}
+            onChange={handleChange("NIC")}
+            helperText={error.NIC ? dirty.NIC : ""}
+          />
+        </Grid>
+
         <Grid item xs={12} sm={12}>
           <FormControl>
             <FormLabel>Status</FormLabel>
-            <RadioGroup name="status" value={status} onClick={handleOnChange}>
+            <RadioGroup
+              name="status"
+              value={value.status}
+              onChange={handleChange("status")}
+            >
               <FormControlLabel
                 value="active"
+                disabled={disable_fields}
                 control={<Radio />}
                 label="Active"
               />
               <FormControlLabel
                 value="inactive"
+                disabled={disable_fields}
                 control={<Radio />}
                 label="Inactive"
               />
@@ -155,6 +453,25 @@ export default function AddressForm(props) {
           </FormControl>
         </Grid>
       </Grid>
+      <div className={classes.buttons}>
+        <Button
+          className={classes.button}
+          variant="contained"
+          color="secondary"
+          onClick={handleClear}
+        >
+          Clear
+        </Button>
+        <Button
+          variant="contained"
+          disabled={disable_fields}
+          color="primary"
+          className={classes.button}
+          onClick={handleSubmit}
+        >
+          {user_update === true ? "Update" : "Create"}
+        </Button>
+      </div>
     </React.Fragment>
   );
 }
